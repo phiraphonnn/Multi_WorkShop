f9c5495861c581eebff9ac8182898f42a73767c4
somebody54569 <sorawis.leep@bumail.net>
2024-01-31 16:00:44 +0700

    02 - Prepare Asset for Multiplayer Project

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    [SerializeField] private InputReader inputReader;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputReader.MoveEvent += HandleMove;
+    }
+
+    private void OnDestroy()
+    {
+        inputReader.MoveEvent -= HandleMove;
+    }
+
+    // Update is called once per frame
+    private void HandleMove(Vector2 movement)
+    {
+        Debug.Log(movement);
+    }
+}
