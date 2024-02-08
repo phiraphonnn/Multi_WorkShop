ff29d48b735b5c2a2e990f6f4171dd06da302a62
SuNnY54569 <sorawis.leep@bumail.net>
2024-02-09 00:11:22 +0700

    Coin, CoinSpawner, CoinWallet

---
+++ Assets/Scripts/Core/Coin/CoinSpawner.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class CoinSpawner : NetworkBehaviour
+{
+    [SerializeField] private RespawningCoin coinPrefab;
+    [SerializeField] private int maxCoins = 50;
+    [SerializeField] private int coinValue = 10;
+    [SerializeField] private Vector2 xSpawnRange;
+    [SerializeField] private Vector2 ySpawnRange;
+    [SerializeField] private LayerMask layerMask;
+
+    private Collider2D[] coinBuffer = new Collider2D[1];
+    
+    private float coinRadius;
+
+    public override void OnNetworkSpawn()
+    {
+        if (!IsServer) { return; }
+        coinRadius = coinPrefab.GetComponent<CircleCollider2D>().radius;
+
+        for (int i = 0; i < maxCoins; i++)
+        {
+            SpawnCoin();
+        }
+    }
+
+    private void SpawnCoin()
+    {
+        RespawningCoin coinInstance =  Instantiate(
+            coinPrefab,
+            GetspawnPoint(),
+            Quaternion.identity);
+        
+        coinInstance.SetValue(coinValue);
+        coinInstance.GetComponent<NetworkObject>().Spawn();
+
+        coinInstance.Oncollected += HandleCoinCollected;
+    }
+
+    private void HandleCoinCollected(RespawningCoin coin)
+    {
+        coin.transform.position = GetspawnPoint();
+        coin.Reset();
+    }
+
+    private Vector2 GetspawnPoint()
+    {
+        float x = 0;
+        float y = 0;
+        while (true)
+        {
+            x = Random.Range(xSpawnRange.x, ySpawnRange.y);
+            y = Random.Range(xSpawnRange.x, ySpawnRange.y);
+            Vector2 spawnPoint = new Vector2(x, y);
+            
+            ContactFilter2D contactFilter2D = new ContactFilter2D();
+            contactFilter2D.layerMask = layerMask;
+            int numColliders = Physics2D.OverlapCircle(spawnPoint, coinRadius, contactFilter2D, coinBuffer);
+            if (numColliders == 0)
+            {
+                return spawnPoint;
+            }
+        }
+    }
+}
